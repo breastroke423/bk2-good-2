8555ed0d9f52a1c8fc2bd1855196564605b29363
Mitsuhiro.Akeda <mitsuhiro.akeda@MitsuhiroAkedanoMacBook-Air.local>
2020-08-09 14:11:22 +0900

    0809

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,21 +3,24 @@ class PostCommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    # redirect
   end
 
 
   def destroy
-    @post_comment = PostComment.find(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @post_comment = PostComment.find_by(id: params[:id], book_id: params[:book_id])
     @post_comment.destroy
-    redirect_to request.referer
+    @post_comments = book.post_comments
+    # redirect_to request.referer
   end
 
 private
+
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
